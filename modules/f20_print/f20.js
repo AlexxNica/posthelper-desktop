37222bdc212a1ecb283baf1647472cae8a84b04c
Ap.t <mzkbarjs@gmail.com>
2017-08-05 01:47:38 +0300

    f20 table build

--- modules/f20_print/f20.js
+++ modules/f20_print/f20.js
@@ -9,10 +9,27 @@ function addIndex() {
   indexTableDocument.append('<tr><td>' + index + '</td></tr>');
 }
 
+function sendToPrint() {
+  var toPush = '';
+  if (indexList.length % 6 !== 0) {
+    while (indexList.length % 6 !== 0) {
+      indexList.push(toPush);
+    }
+  }
+  nw.Window.open('modules/f20_print/print.html', {}, function(win) {
+    win.maximize();
+    // data is the list
+    win.window.data = indexList;
+  });
+}
+
 function initF20() {
   indexInput = $('#indexInput');
   indexTableDocument = $('#indexTable');
   $('#addIndexButton').button().click( function (event) {
     addIndex();
+  });
+  $('#printIndexesButton').button().click( function (event) {
+    sendToPrint();
   });
 }
