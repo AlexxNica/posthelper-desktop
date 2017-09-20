9b4cc56e1eacba159db854d7f1509c5ba8143ee9
apt-ma <mzkbarjs@gmail.com>
2017-09-20 23:55:05 +0300

    added note dialog

--- modules/storage-remainder/main.js
+++ modules/storage-remainder/main.js
@@ -1,5 +1,21 @@
+const dialogs = {};
+
 function moduleInit() {
+  
   $('#addNoteButton').button().click( (event) => {
     alert('Не создано сохранение');
+    dialogs.addNote.dialog('open');
+  });
+
+  dialogs.addNote = $('#addNoteDialog').dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+      Отмена: () => {
+        dialogs.addNote.dialog('close');
+      }
+    }
   });
 }
